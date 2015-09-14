ad67374fdedaeb09a161eb765eda652427ccf32b
Tony Bowden <tony@tmtm.com>
2015-09-14 15:09:41 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,42 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+    
+  end
+end
+
+def noko_for(url)
+  # raw = open(url).read.force_encoding('euc-kr').encode('utf-8')
+  # Nokogiri::HTML(raw)
+  Nokogiri::HTML(open(url).read, nil, 'euc-kr') 
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  box = noko.css('#contentSize')
+  box.xpath('text()').drop(1).each_with_index do |line, i|
+    matched = line.text.tidy.match(/제(.*?)호 (.*?)선거구 ?(.*?)$/) or binding.pry
+    got = matched.captures
+    data = { 
+      id: got[0],
+      name: got[2],
+      area: got[1],
+      term: 13,
+      source: url,
+    }
+    # puts data
+    ScraperWiki.save_sqlite([:id, :term], data)
+  end
+end
+
+scrape_list('http://www.dailynk.com/korean/read.php?num=102886&cataId=nk00100')
